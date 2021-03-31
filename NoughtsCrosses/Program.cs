32e7ab22f8950172286b866b6607c66955446ff7
snikeps <snikeps@ya.ru>
2021-03-31 08:09:58 +0300

    pre-final stage

--- NoughtsCrosses/Program.cs
+++ NoughtsCrosses/Program.cs
@@ -6,7 +6,9 @@ namespace NoughtsCrosses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game game = new Game();
+            game.PlayGame();
+            Console.WriteLine("Game over");
         }
     }
 }
