37ecda7ebd1a8f8a7bceb6ab483531024827d7ff
snikeps <snikeps@ya.ru>
2021-04-12 16:33:10 +0300

    new proj and processing from csv

---
+++ Cars/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+
+            var query = cars.Take(40);
+
+            foreach(var car in query)
+            {
+                Console.WriteLine(car.Name);
+            }
+
+        }
+
+        private static List<Cars> ProcessFile(string path)
+        {
+            //return
+            //    File.ReadAllLines(path)
+            //        .Skip(1)
+            //        .Where(line => line.Length > 1)
+            //        .Select(Cars.ParseFromCsv)
+            //        .ToList();
+
+            var query =
+                from line in File.ReadAllLines(path).Skip(1)
+                where line.Length > 1
+                select Cars.ParseFromCsv(line);
+
+            return query.ToList();
+        }
+    }
+}
--- Queries/Program.cs
+++ Queries/Program.cs
@@ -8,6 +8,13 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+            var numbers = MyLinq.Random().Where( n => n > 0.5).Take(10);
+
+            foreach ( var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
             var movies = new List<Movie>
             {
                 new Movie { Title = "The Dark Knight",      Rating = 8.9f, Year = 2008 },
@@ -16,7 +23,15 @@ namespace Queries
                 new Movie { Title = "Star Wars V",          Rating = 8.7f, Year = 1980 }
             };
 
-            var query = movies.Filter(m => m.Year > 2000);
+
+            var query = from movie in movies
+                        where movie.Year > 2000
+                        orderby movie.Rating descending
+                        select movie;
+
+            //var query = movies.Filter(m => m.Year > 2000)
+            //                    .OrderByDescending(m => m.Rating);
+            Console.WriteLine(query.Count());
 
             foreach (var movie in query)
             {
