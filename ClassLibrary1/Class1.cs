69348fbea6658bed37b6bba32f29a3ae1023bdf0
snikeps <snikeps@ya.ru>
2021-04-13 08:52:18 +0300

    new testproject calculating Squres

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public static class Class1
+    {
+        public static double SCircle(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        public static float STriangle(float a, float b, float c)
+        {
+            float p = (a + b + c) / 2;
+            try
+            {
+                return (float)Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            }
+            catch
+            {
+                throw new Exception("Input values should be greater than zero");
+            }
+        }
+    }
+}
---
+++ testcase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace testcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = Class1.SCircle(1);
+            Console.WriteLine(circle);
+
+            var triangle = Class1.STriangle(3, -4, 5);
+            Console.WriteLine(triangle);
+        }
+
+
+    }
+}
