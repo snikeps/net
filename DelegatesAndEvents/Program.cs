b308bcd774ddcfe7cb41797864860079eea84133
snikeps <snikeps@ya.ru>
2021-04-26 09:11:13 +0300

    using delegates dynamically

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -10,20 +10,27 @@ namespace DelegatesAndEvents
             WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
             WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
 
-            del1(5, WorkType.Golf);
-            del2(10, WorkType.GenerateReports);
+            //del1(5, WorkType.Golf);
+            //del2(10, WorkType.GenerateReports);
+
+            DoWork(del2);
 
             Console.Read();
         }
 
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+
         static void WorkPerformed1(int hours, WorkType workType)
         {
-            Console.WriteLine("WorkPermormed1 called");
+            Console.WriteLine($"WorkPermormed1 called, hours = {hours}, WorkType = {workType}");
         }
 
         static void WorkPerformed2(int hours, WorkType workType)
         {
-            Console.WriteLine("WorkPerformed2 called");
+            Console.WriteLine($"WorkPerformed2 called, hours = {hours}, WorkType = {workType}");
         }
     }
 
