318955775bd51a27785968147295a3585ff152e6
snikeps <snikeps@ya.ru>
2021-04-26 09:04:38 +0300

    project created with 2 delegates

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            del1(5, WorkType.Golf);
+            del2(10, WorkType.GenerateReports);
+
+            Console.Read();
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPermormed1 called");
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called");
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
