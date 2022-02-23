ea61ed6e93c5133da803ceea83afe932fede62da
Anas Iusupov <Anas_Iusupov@epam.com>
2022-02-23 15:17:57 +0300

    implementation of Rx.Net

--- WeatherApp/WeatherApp/Program.cs
+++ WeatherApp/WeatherApp/Program.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace WeatherApp
@@ -11,10 +11,13 @@ namespace WeatherApp
     {
         static HttpClient client = new HttpClient();
 
+        const string url = "http://api.weatherapi.com/v1/current.json?key=8a10fccef8c64fe4be8174632221502&q=Kazan&aqi=no";
+
         static void ShowWeather(Weather weather)
         {
             Console.WriteLine($"Location name: {weather.Location.Name}\n" +
                               $"Last updated time: {weather.Current.Last_updated}\n" +
+                              $"Request sent time: {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}\n" +
                               $"Humidity: {weather.Current.Humidity}");
         }
 
@@ -33,17 +36,19 @@ namespace WeatherApp
         }
 
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            RunAsync().GetAwaiter().GetResult();
+            IObservable<long> obs  = Observable.Timer(DateTimeOffset.Now.AddSeconds(1), TimeSpan.FromSeconds(2)).Take(10);
+
+            obs.Subscribe(_ => RunAsync().GetAwaiter().GetResult());
+
+            await new TaskCompletionSource<object>().Task;
         }
 
         static async Task RunAsync()
         {
             try
             {
-                var url = "http://api.weatherapi.com/v1/current.json?key=8a10fccef8c64fe4be8174632221502&q=Kazan&aqi=no";
-
                 var weather = await GetWeatherAsync(url);
 
                 ShowWeather(weather);
