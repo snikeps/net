5d09c9d9e1f14fc63224430025a83dba9786c39c
Anas Yusupov <snikeps@ya.ru>
2022-07-20 15:06:07 +0300

    Pagination kata

---
+++ TestApps/CodeWars/PagnationHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class PagnationHelper<T>
+    {
+        private IList<T> _collection;
+        private int _itemsPerPage;
+        public PagnationHelper(IList<T> collection, int itemsPerPage)
+        {
+            _collection = collection;
+            _itemsPerPage = itemsPerPage;
+        }
+
+        public int ItemCount
+        {
+            get
+            {
+                return _collection.Count;
+            }
+        }
+
+        public int PageCount
+        {
+            get
+            {
+                return (int)Math.Ceiling(_collection.Count / Convert.ToDecimal(_itemsPerPage));
+            }
+        }
+
+        public int PageItemCount(int pageIndex)
+        {
+            if (pageIndex >= PageCount || pageIndex < 0)
+                return -1;
+
+            return _collection.Skip(pageIndex * _itemsPerPage).Take(_itemsPerPage).Count();
+        }
+
+        public int PageIndex(int itemIndex)
+        {
+            if (itemIndex >= ItemCount || itemIndex < 0)
+                return -1;
+
+            return itemIndex / _itemsPerPage;
+        }
+    }
+}
--- TestApps/CodeWars/Program.cs
+++ TestApps/CodeWars/Program.cs
@@ -8,60 +8,16 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Kata.IsInteresting(98, new List<int>() { 1337, 256 }));
-        }
-
-    }
-
-    public static class Kata
-    {
-        public static int IsInteresting(int number, List<int> awesomePhrases)
-        {
-            if (number < 98)
-                return 0;
-
-            if (CheckInput(number, awesomePhrases))
-                return 2;
-
-            if (CheckInput(number + 1, awesomePhrases)
-                || CheckInput(number + 2, awesomePhrases))
-                return 1;
-
-            return 0;
-        }
-
-        private static bool CheckInput(int number, List<int> awesomePhrases)
-        {
-            string num = number.ToString();
-
-            if (num.Length < 3)
-                return false;
-
-            // The digits match one of the values in the awesomePhrases array
-            if (awesomePhrases.Any(x => x == number))
-                return true;
-
-            // Any digit followed by all zeros
-            if (number % (Math.Pow(10, num.Length - 1)) == 0)
-                return true;
-
-            // Every digit is the same number
-            if (num.All(x => x == num[0]))
-                return true;
-
-            // The digits are a palindrome
-            if (num.Take(num.Length / 2).SequenceEqual(num.TakeLast(num.Length / 2).Reverse()))
-                return true;
-
-            // The digits are sequential, incementing / decrementing
-            var num1 = num.Skip(1).Select(x => (Convert.ToInt32(x) + 2) % 10);
-            var num2 = num.SkipLast(1).Select(x => (Convert.ToInt32(x) + 2) % 10);
-
-            if (num1.SequenceEqual(num2.Select(x => ((x - 1) % 10)))
-                || num1.SequenceEqual(num2.Select(x => ((x + 1) % 10))))
-                return true;
-
-            return false;
+            var helper = new PagnationHelper<char>(new List<char> { 'a', 'b', 'c', 'd', 'e', 'f' }, 4);
+            Console.WriteLine($"PageCount() = {helper.PageCount}");
+            Console.WriteLine($"PageCount() = {helper.ItemCount}");
+            Console.WriteLine($"PageItemCount(0) = {helper.PageItemCount(0)}");
+            Console.WriteLine($"PageItemCount(1) = {helper.PageItemCount(1)}");
+            Console.WriteLine($"PageItemCount(2) = {helper.PageItemCount(2)}");
+            Console.WriteLine($"PageIndex(5) = {helper.PageIndex(5)}");
+            Console.WriteLine($"PageIndex(2) = {helper.PageIndex(2)}");
+            Console.WriteLine($"PageIndex(20) = {helper.PageIndex(20)}");
+            Console.WriteLine($"PageIndex(-10) = {helper.PageIndex(-10)}");
         }
     }
 }
