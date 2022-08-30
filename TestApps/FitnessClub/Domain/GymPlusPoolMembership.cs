e2cca6fc496220eea939c26a2808edd99f50911b
Anas Yusupov <snikeps@ya.ru>
2022-08-30 14:57:46 +0300

    Factory design pattern

---
+++ TestApps/FitnessClub/Domain/GymPlusPoolMembership.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnessClub.Domain
+{
+    internal class GymPlusPoolMembership : IMembership
+    {
+        private readonly string _name;
+        private readonly decimal _price;
+
+        public GymPlusPoolMembership(decimal price)
+        {
+            _name = "Gym + Pool Membership";
+            _price = price;
+        }
+
+        public string Name => _name;
+        public string Description { get; set; }
+        public decimal GetPrice() => _price;
+    }
+}
